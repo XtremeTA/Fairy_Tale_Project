617f15587ef075356316e2db82a1e603ae028784
XtremeTA <164788394+XtremeTA@users.noreply.github.com>
2024-11-14 13:41:50 -0600

    Update Once_Upon_An_Arduino.ino

--- Once_Upon_An_Arduino.ino
+++ Once_Upon_An_Arduino.ino
@@ -1,10 +1,16 @@
 #include <Servo.h>
 
-Servo myservo;
-const int buttonPin = 10;
-int buttonState = 0;
-int prevButtonState = 0;
+Servo firstservo;
+Servo secondservo;
+const int potionPin = 10;
+const int victimPin = 12;
+int potionState = 0;
+int prevPotionState = 0;
+int victimState = 0;
+int prevVictimState = 0;
 bool A = true;
+bool B = false;
+bool C = false;
 
 
 /* This is the code for the first scene
@@ -14,27 +20,64 @@ bool A = true;
 */
 
 void setup() {
-  myservo.attach(9);
-  myservo.write(0);
-  pinMode(buttonPin, INPUT);
+  firstservo.attach(9);
+  firstservo.write(0);
+  secondservo.attach(11);
+  secondservo.write(180);
+  pinMode(potionPin, INPUT);
+  pinMode(victimPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState != prevButtonState){
-    if (buttonState == LOW) {
+  potionState = digitalRead(potionPin);
+  victimState = digitalRead(victimPin);
+  """
+  First Scene
+  """
+
+  if (potionState != prevPotionState){
+    if (potionState == LOW) {
       A = false;
-      Serial.println("Start Part B");
+      Serial.println("Start Potion Part");
     }
   }
   if (A == false){
-    if (buttonState != prevButtonState){
-      if (buttonState == HIGH) {
-        myservo.write(180);
+    if (potionState != prevPotionState){
+      if (potionState == HIGH) {
+        firstservo.write(180);
+        B = true;
       }
     }
   }
-  prevButtonState = buttonState;
+
+  """
+  Second Scence
+  """
+
+  if (B == true){
+    if (victimState != prevVictimState){
+      if (victimState = HIGH) {
+        for (int i = 0; i < 3; i++){
+          secondservo.write(0);
+          delay(500);
+          secondservo.write(180);
+          delay(500);
+        }
+        B = false;
+        C = true;
+      }
+    }
+  }
+  """
+  Third Scene (in progress)
+  """
+  /*
+  if (C == true) {
+
+  }
+  */
+  prevPotionState = potionState;
+  prevVictimStae = victimState;
 
 }
