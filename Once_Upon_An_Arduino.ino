cbbf49d1b884bf507a712d401433db939be4ddb3
XtremeTA <164788394+XtremeTA@users.noreply.github.com>
2024-11-14 10:45:20 -0600

    Update Once_Upon_An_Arduino.ino

--- Once_Upon_An_Arduino.ino
+++ Once_Upon_An_Arduino.ino
@@ -1,15 +1,21 @@
 #include <Servo.h>
 
 Servo myservo;
-const int buttonPin = 2;
+const int buttonPin = 10;
 int buttonState = 0;
 int prevButtonState = 0;
 bool A = true;
 
-//Code for First Scene
+
+/* This is the code for the first scene
+   The potion would be on the table already,
+   and when someone takes it off and puts it back,
+   the servo would turn 180 degrees so Jekyll would turn into Hyde.
+*/
 
 void setup() {
   myservo.attach(9);
+  myservo.write(0);
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
 }
